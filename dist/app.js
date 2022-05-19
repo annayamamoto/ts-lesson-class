248c80907e62ee1f1ad9b8274e19581e8e83ad47
annayamamoto <anxxx.014@icloud.com>
2022-05-19 16:26:03 +0900

    comi

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Department("Acounting");
+console.log(accounting);
 //# sourceMappingURL=app.js.map
